f7dc287a6dee110fa10574b021b2405a01bfd3de
shivanshgoel-12 <53016763+shivanshgoel-12@users.noreply.github.com>
2020-01-29 21:58:09 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,19 +1,27 @@
 import cv2
-import time
-from imutils.video import VideoStream
+import numpy as np 
+#import time
+#from imutils.video import VideoStream
 #t,y = cv2.VideoCapture(0).read()
 #cv2.imshow('frame',y)
 #cv2.waitKey(0)
 vs = cv2.VideoCapture(0)
-time.sleep(2.0)
-i=0
+#time.sleep(2.0)
+#i=0
 while True:
-    i+=1
-    print("HI")
-    frame = vs.read()
-    cv2.imshow('Screen',frame[1])
-    cv2.waitKey(1)
-    if i == 1000:
-        break
-vs.stop()
-#comment
+    res,frame = vs.read()
+    if res==True:
+        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        lower=np.array([65,60,60])
+        upper=np.array([80,255,255])
+        mask=cv2.inRange(hsv,lower,upper)
+        green=cv2.bitwise_and(frame,frame,mask=mask)
+        cv2.imshow('Mask',mask)
+        cv2.imshow('Screen',frame)
+        cv2.imshow('Green',green)
+        k=cv2.waitKey(1)
+        if k== 27:
+            break
+vs.release()
+
+
