54586cba2025b99cc4f08ec20fe005423404561f
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-01-29 13:06:10 +0530

    Video Capturing

--- detection.py
+++ detection.py
@@ -1,2 +1,19 @@
 import cv2
-cv2.VideoCapture(0)
+import time
+from imutils.video import VideoStream
+#t,y = cv2.VideoCapture(0).read()
+#cv2.imshow('frame',y)
+#cv2.waitKey(0)
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+i=0
+while True:
+    i+=1
+    print("HI")
+    frame = vs.read()
+    cv2.imshow('Screen',frame[1])
+    cv2.waitKey(1)
+    if i == 1000:
+        break
+vs.stop()
+cv2.stopAllWindows()
