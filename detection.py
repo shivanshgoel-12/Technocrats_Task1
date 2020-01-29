78cebb093d4ba2c61750ddadecd0420c2cf90229
karanysingh <karan124d@gmail.com>
2020-01-29 13:10:16 +0530

    Video Capturing

--- detection.py
+++ detection.py
@@ -4,7 +4,7 @@ from imutils.video import VideoStream
 #t,y = cv2.VideoCapture(0).read()
 #cv2.imshow('frame',y)
 #cv2.waitKey(0)
-vs = VideoStream(src=0).start()
+vs = cv2.VideoCapture(0)
 time.sleep(2.0)
 i=0
 while True:
