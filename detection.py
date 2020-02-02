43e5f36d192c11237bb11cfdea91b0cb7235a7cb
shivanshgoel-12 <53016763+shivanshgoel-12@users.noreply.github.com>
2020-02-02 15:24:28 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,25 +1,22 @@
-import cv2
-import numpy as np 
-#import time
-#from imutils.video import VideoStream
-#t,y = cv2.VideoCapture(0).read()
-#cv2.imshow('frame',y)
-#cv2.waitKey(0)
-vs = cv2.VideoCapture(0)
-#time.sleep(2.0)
-#i=0
-while True:
-    res,frame = vs.read()
-    if res==True:
-        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        lower=np.array([65,60,60])
-        upper=np.array([80,255,255])
-        mask=cv2.inRange(hsv,lower,upper)
-        green=cv2.bitwise_and(frame,frame,mask=mask)
-        cv2.imshow('Mask',mask)
-        cv2.imshow('Screen',frame)
-        cv2.imshow('Green',green)
-        k=cv2.waitKey(1)
-        if k== 27:
-            break
-vs.release()
+import cv2 
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+lower_green = np.array([45,140,50]) 
+upper_green = np.array([75,255,255])
+while(True):
+    k=0 ## Signal for arduino as no green ball
+    success,frame = cap.read()  
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.medianBlur(hsv,5)
+    green = cv2.inRange(hsv, lower_green, upper_green)
+    circlesgreen = cv2.HoughCircles(green,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+    if circlesgreen is not None:
+            print ("Green ball Found")
+            k=1  ## Signal for arduino as Green Ball
+    cv2.imshow('face',frame)
+    k=cv2.waitKey(1)
+    if k == ord('q'):
+        break
+cv2.destroyAllWindows()
